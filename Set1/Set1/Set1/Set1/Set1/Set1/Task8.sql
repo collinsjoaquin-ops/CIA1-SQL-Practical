b18e7e2f200bd4f702612b20b47e2344cc33aed0
collinsjoaquin-ops <collins.joaquin@btech.christuniversity.in>
2026-07-20 10:04:20 +0530

    Create Task8.sql

---
+++ Set1/Set1/Set1/Set1/Set1/Set1/Task8.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (student_id, name, course, marks, city, email) VALUES
+(2, 'Rahul Mehta', 'BCA', 76, 'Mumbai', 'rahul.mehta@example.com'),
+(3, 'Sneha Iyer', 'BSc Data Science', 92, 'Chennai', 'sneha.iyer@example.com'),
+(4, 'Kabir Singh', 'BBA', 65, 'Delhi', 'kabir.singh@example.com');
